50d23487398f2ae044b949bc19c259aa95ceda09
algostrat <8900863+algostrat@users.noreply.github.com>
2021-04-30 20:43:01 -0400

    updated global and server

---
+++ global.R
@@ -0,0 +1,8 @@
+## Check packages ##
+if (!require('DT')) install.packages("DT")
+if (!require('shiny')) install.packages("shiny")
+if (!require('shinythemes')) install.packages("shinythemes")
+if (!require('shinyFiles')) install.packages("shinyFiles")
+if (!require('ggplot2')) install.packages("ggplot2")
+if (!require('ISLR')) install.packages("ISLR")
+
--- server.R
+++ server.R
@@ -1,15 +1,14 @@
+source("global.R")
 library(ISLR)
 library(shiny)
 library(ggplot2)
-data("College")
-attach(College)
-cols = colnames(College)
-cols = cols[cols !="Private"]
 library(shinythemes)
 library(DT)
 
+data("College")
+cols = colnames(College)
+cols = cols[cols !="Private"]
 
-cols
 names = c("Applications recieved","Applications accepted", "Number of new enrollees", "Number of top 10% students",
           "Number of top 25% students", "Fulltime undergrads", "Partime undergrads", "Out of state tuition",
           "Room and Board costs", "Book costs", "Estimated personal expenditure", "% of Phd faculty",
@@ -26,9 +25,10 @@ ui <- fluidPage(
   theme = shinytheme('united'),
   navbarPage("Explore linear relationships in college data")
   ,
-  titlePanel("Model Options"),
+  titlePanel("U.S. News and World Report's College Data"),
   sidebarLayout(
     sidebarPanel(
+      h3("Model Options"),
       selectInput(inputId = "regressor", "Select regressor",
                   names,selected = "Out of state tuition"),
       selectInput(inputId = "predictor", "Select predictor",
@@ -43,11 +43,45 @@ ui <- fluidPage(
       #)
     ),
     mainPanel(
-     # h1("regression"),
+      h3("Regression plot"),
       plotOutput("plot"),
-      DT::dataTableOutput("mytable",height="400px")
+      plotOutput("residualDiagnostics"),
+      h3("Full dataset"),
+      DT::dataTableOutput("mytable",height="400px"),
+      h3("Variables"),
+      HTML("<ul>
+             <li>Private = A factor with levels No and Yes indicating private or public university</li>
+             <li>Apps = Number of applications received</li>
+             <li>Accept = Number of applications accepted</li>
+             <li>Enroll = Number of new students enrolled</li>
+             <li>Top10perc = Pct. new students from top 10% of H.S. class</li>
+             <li>Top25perc = Pct. new students from top 25% of H.S. class</li>
+             <li>F.Undergrad = Number of fulltime undergraduates</li>
+             <li>P.Undergrad = Number of parttime undergraduates</li>
+             <li>Outstate = Out-of-state tuition</li>
+             <li>Room.Board = Room and board costs</li>
+             <li>Books = Estimated book costs</li>
+             <li>Personal = Estimated personal spending</li>
+             <li>PhD = Pct. of faculty with Ph.D.'s</li>
+             <li>Terminal = Pct. of faculty with terminal degree</li>
+             <li>S.F.Ratio = Student/faculty ratio</li>
+             <li>perc.alumni = Pct. alumni who donate</li>
+             <li>Expend = Instructional expenditure per student</li>
+             <li>Grad.Rate = Graduation rate</li>
+           </ul>"),
+      h3("Source"),
+      p("This dataset was taken from the StatLib library which is maintained
+        at Carnegie Mellon University. The dataset was used in the ASA 
+        Statistical Graphics Section's 1995 Data Analysis Exposition."),
+      h3("Additional information"),
+      tags$a(href="https://github.com/algostrat/shinyappCollegeDataset", "Github project repository,"),
+      tags$a(href="https://www.statlearning.com/", "StatLearning.com,"),
+      tags$a(href="https://rdrr.io/cran/ISLR/man/College.html", "rdrr.io/cran/ISLR/man/College.html"),
       
-      
+      h3("References"),
+      p("James, G., Witten, D., Hastie, T., and Tibshirani, R. 
+        (2013) An Introduction to Statistical Learning with applications in R")
+
       )
     )
   )
